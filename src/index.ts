0a10a8d2577000016efcd66a9a005d4fa6ba8de3
justorez <alonezero@foxmail.com>
2024-02-21 16:18:53 +0800

    build: use microbundle to make building easy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export function hi(name?: string) {
     console.log('Hello', name)
 }
 
-hi('Justorez')
+hi(__dirname + ': Justorez')
