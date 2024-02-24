a362c5d7bff75f87cf623289f20f6257a1888f95
justorez <alonezero@foxmail.com>
2024-02-24 23:04:15 +0800

    build: fix build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import { readFileSync } from 'node:fs'
+import { readJsonSync } from 'fs-extra/esm'
 import { builtinModules } from 'node:module'
 import { defineConfig } from 'rollup'
 import json from '@rollup/plugin-json'
@@ -7,10 +7,10 @@ import commonjs from '@rollup/plugin-commonjs'
 import esbuild from 'rollup-plugin-esbuild'
 import { visualizer } from 'rollup-plugin-visualizer'
 
-const pkg = JSON.parse(
-    readFileSync(new URL('./package.json', import.meta.url), 'utf8')
-)
+const pkg = readJsonSync('./package.json')
 
+// rimraf dist && rollup -c rollup.config.js
+// more faster than microbundle
 export default defineConfig({
     input: {
         index: 'src/index.ts'
@@ -40,10 +40,9 @@ export default defineConfig({
         // https://npmmirror.com/package/@rollup/plugin-node-resolve
         nodeResolve({
             preferBuiltins: false
-            // 为什么设置之后就报错？
             // [!] (plugin commonjs--resolver)
             // TypeError: The "path" argument must be of type string or an instance of URL. Received null
-            // exportConditions: ['node']
+            // exportConditions: ['node'] // Why does this config cause error?
         }),
         commonjs(),
         visualizer()
