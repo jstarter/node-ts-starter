8d1e44d27d72424582d0d657fa77dcf18f9aff99
justorez <alonezero@foxmail.com>
2024-04-16 15:45:08 +0800

    feat: debug config in vscode

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
+export const fileURL = import.meta.url
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-export function hi(name?: string) {
-    console.log('Hello', name)
+export function hi(...args: string[]) {
+    console.log('Hello', ...args)
 }
 
-hi(__dirname + ': Justorez')
+hi(__dirname, 'Justorez')
